878c5a99436e12ec95865d5ff10431522d45d5a3
EgorTovstik <eg1707or2003@gmail.com>
2023-12-16 18:32:50 +0700

    Y

--- blog/src/Form/ArticleType.php
+++ blog/src/Form/ArticleType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Article;
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +16,14 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+            ->add('title', TextType::class, [
+                'label' => 'Название статьи',
+                'required' => true,
+            ])
+            ->add('body', TextareaType::class,[
+                'label' => 'Текст статьи',
+                'required' => true,
+            ])
         ;
     }
 
